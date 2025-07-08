ed42e4a182597355a2670951cc8f396a85466ac4
pureum1215 <pureum1215@naver.com>
2025-07-08 17:42:39 +0900

    임시 지도 페이지 생성

---
+++ frontend/src/pages/map/RestaurantsMapPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RestaurantMap from "../../components/map/restaurant/RestaurantMap";
+
+function RestaurantPage() {
+  return (
+    <div>
+      <h1>일반 음식점 지도</h1>
+      <RestaurantMap />
+    </div>
+  );
+}
+
+export default RestaurantPage;

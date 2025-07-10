c78902ce8f13b2326de774ac42c93ef0ed9a7992
pureum1215 <pureum1215@naver.com>
2025-07-10 15:18:48 +0900

    위반 건수 출력 기능 오류 수정

--- backend/src/main/java/kr/map/food/mapper/apiData/RestaurantTypeMapper.java
+++ backend/src/main/java/kr/map/food/mapper/apiData/RestaurantTypeMapper.java
@@ -5,6 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface RestaurantTypeMapper {
 
-    int findTypeIdx(String TYPENAME);
+    Integer findTypeIdx(String TYPENAME);
     
 }

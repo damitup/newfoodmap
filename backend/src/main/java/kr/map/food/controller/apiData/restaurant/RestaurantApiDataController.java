bcb4715f8398352572b9605aff8f8297cd0bf3a7
pureum1215 <pureum1215@naver.com>
2025-07-14 09:39:34 +0900

    restaurant api 출력 부분 삭제

--- backend/src/main/java/kr/map/food/controller/apiData/restaurant/RestaurantApiDataController.java
+++ backend/src/main/java/kr/map/food/controller/apiData/restaurant/RestaurantApiDataController.java
@@ -1,33 +1,25 @@
 package kr.map.food.controller.apiData.restaurant;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.map.food.domain.apiData.restaurant.RestaurantApiDTO;
-import kr.map.food.mapper.apiData.RestaurantApiDataMapper;
+import kr.map.food.service.apiData.restaurant.RestaurantApiDataService;
 
 @RestController
 @RequestMapping("/api")
 public class RestaurantApiDataController {
 
-    private final RestaurantApiDataMapper restaurantMapper;
+    private final RestaurantApiDataService restaurantService;
     
-    public RestaurantApiDataController(RestaurantApiDataMapper restaurantMapper) {
-        this.restaurantMapper = restaurantMapper;
+    public RestaurantApiDataController(RestaurantApiDataService restaurantService) {
+        this.restaurantService = restaurantService;
     }
 
-    @GetMapping("/restaurant")
-    public List<RestaurantApiDTO> getAll() {
-        return restaurantMapper.selectAll(); 
+    @PostMapping("/restaurant/collect")
+    public void collect() {
+        restaurantService.collectAllGuData();
     }
 
-    @GetMapping("/restaurant/{RESIDX}")
-    public RestaurantApiDTO getById(@PathVariable String RESIDX) {
-        return restaurantMapper.selectById(RESIDX);
-    }
     
 }
